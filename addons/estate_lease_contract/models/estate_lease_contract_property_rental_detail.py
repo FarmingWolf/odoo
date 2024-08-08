51e3d3df895f11bb7ebe986a9cdb433397292d4f
Donsun <2165693@qq.com>
2024-08-08 19:25:52 +0800

    单做租金催缴界面

--- addons/estate_lease_contract/models/estate_lease_contract_property_rental_detail.py
+++ addons/estate_lease_contract/models/estate_lease_contract_property_rental_detail.py
@@ -30,6 +30,16 @@ class EstateLeaseContractPropertyRentalDetail(models.Model):
     description = fields.Char(string="描述")
     active = fields.Boolean(default=True)
 
+    renter_id = fields.Many2one('res.partner', string="承租人", related='contract_id.renter_id', readonly=True)
+    renter_id_phone = fields.Char(string="电话", related='contract_id.renter_id.phone', readonly=True)
+    renter_id_mobile = fields.Char(string="手机", related='contract_id.renter_id.mobile', readonly=True)
+    rental_arrears = fields.Float(string="欠缴金额", compute='_compute_rental_arrears', readonly=True)
+
+    @api.depends("rental_amount", "rental_received")
+    def _compute_rental_arrears(self):
+        for record in self:
+            record.rental_arrears = round(record.rental_amount - record.rental_received)
+
     @api.depends("rental_amount")
     def _get_default_rental_receivable(self):
         for record in self:
