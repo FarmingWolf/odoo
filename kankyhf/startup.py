8f84db2f10bdbe9d4755f78bed383e0db163a4d3
Donsun <2165693@qq.com>
2025-02-11 12:10:42 +0800

    修订addons参数

--- kankyhf/startup.py
+++ kankyhf/startup.py
@@ -778,7 +778,7 @@ def start_em_server(in_root, in_label, in_bar):
     command.extend(['-d', 'postgres'])
     em_addons = f"utils,parking,estate_registration_addr,estate,estate_lease_contract,event_option,event_extend," \
                 f"operation_contract,accounting_subject,business_items,operation_contract_event_settle_account," \
-                f"wechat,contacts,estate_dashboard,sms_ali,website_estate"
+                f"wechat,contacts,estate_dashboard,sms_ali,website_estate,project,ocr_partner"
     command.extend(['-u', em_addons])
 
     _logger.debug(f"command={command}")
