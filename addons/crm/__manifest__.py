6d0b4502028713ae5d783196ef59bb2bb99fdefc
Donsun <2165693@qq.com>
2025-02-26 14:45:12 +0800

    CRM线索按部门限制权限

--- addons/crm/__manifest__.py
+++ addons/crm/__manifest__.py
@@ -24,6 +24,7 @@
     'data': [
         'security/crm_security.xml',
         'security/ir.model.access.csv',
+        'security/ir.rule.csv',
 
         'data/crm_lead_merge_template.xml',
         'data/crm_lead_prediction_data.xml',
