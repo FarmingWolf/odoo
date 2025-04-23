8e3e7c7cef20d40f12c91323f56f295f4ed38357
说书先生 <14323437+tech491@user.noreply.gitee.com>
2025-04-23 13:47:23 +0800

    排除非计租类型资产面积

--- addons/estate/report/estate_area_report.py
+++ addons/estate/report/estate_area_report.py
@@ -14,6 +14,7 @@ class EstateAreaReport(models.Model):
     property_id = fields.Many2one('estate.property', string="资产（房屋）")
     property_rent_area = fields.Float(string="计租面积")
     property_state = fields.Char(string='资产状态')
+    property_type_id = fields.Many2one('estate.property.type', string='资产类型')
     company_id = fields.Many2one(comodel_name='res.company', default=lambda self: self.env.user.company_id, store=True)
 
     def _select(self):
@@ -23,7 +24,8 @@ class EstateAreaReport(models.Model):
                 t.property_id,
                 t.property_rent_area,
                 CASE WHEN t.property_state = '已租' THEN '已租' ELSE '空置' END as property_state,
-                t.company_id
+                t.company_id,
+                e_p.property_type_id
         """
 
     def _from(self):
@@ -33,12 +35,16 @@ class EstateAreaReport(models.Model):
 
     def _join(self):
         return """
+            JOIN estate_property AS e_p ON e_p.id = t.property_id
+            LEFT JOIN estate_property_type AS e_p_t ON e_p_t.id = e_p.property_type_id
         """
 
     def _where(self):
         return f"""
             WHERE
                 t.status_date = '{date.today() + timedelta(days=-1)}'
+            AND (e_p_t.count_ratio_as_room = true 
+                or e_p_t.count_ratio_as_room is null)
         """
 
     def init(self):
